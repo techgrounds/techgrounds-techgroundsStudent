2532b9fc0e9dd13151ea325971382790226bc2e8
techgroundsStudent <135595563+techgroundsStudent@users.noreply.github.com>
2023-08-14 15:15:49 +0200

    afronding

--- 09_Python/PRG-03/PRG-03.py
+++ 09_Python/PRG-03/PRG-03.py
@@ -16,6 +16,7 @@ print("Data type van 'd':", type_d)
 #Make a new variable x and give it the value b + d. Print the value of x. This will raise an error. Fix it so that print(x) prints a float.
 x = b + float(d) 
 print(x)
+#Write a comment above every line of code that tells the reader what is going on in your script. 
 
 
 
