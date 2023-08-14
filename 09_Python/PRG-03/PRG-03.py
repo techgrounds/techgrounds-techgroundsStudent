436abf6ccc1a1685e03dffb32e0e860632a8f3de
techgroundsStudent <135595563+techgroundsStudent@users.noreply.github.com>
2023-08-14 15:02:31 +0200

    using F

--- 09_Python/PRG-03/PRG-03.py
+++ 09_Python/PRG-03/PRG-03.py
@@ -1,1 +1,19 @@
 #Inject the code
+a = 'int'
+b = 7
+c = False
+d = "18.5"
+#Determine the data types of all four variables (a, b, c, d) using a built in function.
+
+type_a = type(a)
+type_b = type(b)
+type_c = type(c)
+type_d = type(d)
+
+# Print the data types
+print("Data type van 'a':", type_a)
+print("Data type van 'b':", type_b)
+print("Data type van 'c':", type_c)
+print("Data type van 'd':", type_d)
+
+
