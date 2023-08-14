0bd09a196021bac4989d98e20599d73aac12ed7d
techgroundsStudent <135595563+techgroundsStudent@users.noreply.github.com>
2023-08-14 15:29:42 +0200

    einde der einde

--- 09_Python/PRG-03/PRG-03_2.py
+++ 09_Python/PRG-03/PRG-03_2.py
@@ -1,4 +1,6 @@
 #Use the input() function to get input from the user. Store that input in a variable.
 doeEensMee = input("Geef eens wat data in de vorm van een vermakelijke zin met correcte syntax: ")
 print(doeEensMee)
+print(type(doeEensMee))
+
 
