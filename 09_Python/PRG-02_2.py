1d1d2f5b12a826af7c563c5f2611a391c4a56258
techgroundsStudent <135595563+techgroundsStudent@users.noreply.github.com>
2023-08-14 13:50:49 +0200

    afronding

--- 09_Python/PRG-02_2.py
+++ 09_Python/PRG-02_2.py
@@ -1,5 +1,5 @@
 # Create a variable name. The value of name should be your name.
-name = Vincent de Windt
+name = "Vincent de Windt"
 # Print the text “Hello, <your name here>!”. Use name in the print statement.
 print("Hello,", name + "!")
 
