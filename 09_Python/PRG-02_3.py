44e3040eb0d05693f812a32417031c8f347e05f4
techgroundsStudent <135595563+techgroundsStudent@users.noreply.github.com>
2023-08-14 14:14:21 +0200

    afronding

---
+++ 09_Python/PRG-02_3.py
@@ -0,0 +1,12 @@
+#Create a variable and assign a value to it.
+geluksgetal = "777"
+#Print the text “Value 1: <value 1 here>”.
+print("Value 1:", geluksgetal)
+#Change the value of that same variable.
+geluksgetal = "888"
+#Print the text “Value 2: <value 2 here>”.
+print("Value 2:", geluksgetal)
+#Change the value of that same variable.
+geluksgetal = 999
+#Print the text “Value 3: <value 3 here>”.
+print("Value 3:", geluksgetal)
