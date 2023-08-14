02953c7190ef3ded5e98d734eeb4f781c49ce3fe
techgroundsStudent <135595563+techgroundsStudent@users.noreply.github.com>
2023-08-14 13:41:40 +0200

    NL nu

--- 09_Python/PRG-02.py
+++ 09_Python/PRG-02.py
@@ -1,14 +1,14 @@
-# Create two variables x and y and assign values
+# Create two variables x and y and assign numerical values
 x = 777
 y = 888
 
 # Print the values of x and y
-print("Value of x:", x)
-print("Value of y:", y)
+print("Waarde van x", x)
+print("Waarde van y:", y)
 
 # Create a third variable z as the sum of x and y
 z = x + y
 
 # Print the value of z
-print("Value of z:", z)
+print("Waarde van z:", z)
 
