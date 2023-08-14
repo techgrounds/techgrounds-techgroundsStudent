88381f1940037842c390a7adfd7e3e854ae5c4da
techgroundsStudent <135595563+techgroundsStudent@users.noreply.github.com>
2023-08-14 13:48:37 +0200

    Create PRG-02_2.py

---
+++ 09_Python/PRG-02_2.py
@@ -0,0 +1,6 @@
+# Create a variable name. The value of name should be your name.
+name = Vincent de Windt
+# Print the text “Hello, <your name here>!”. Use name in the print statement.
+print("Hello,", name + "!")
+
+
