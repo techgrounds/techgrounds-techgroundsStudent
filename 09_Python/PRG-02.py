c5208674cde267cc1008ebd0399d236e06eaedbf
techgroundsStudent <135595563+techgroundsStudent@users.noreply.github.com>
2023-08-14 13:35:04 +0200

    code basis

---
+++ 09_Python/PRG-02.py
@@ -0,0 +1,14 @@
+# Create two variables x and y and assign values
+x = 777
+y = 888
+
+# Print the values of x and y
+print("Value of x:", x)
+print("Value of y:", y)
+
+# Create a third variable z as the sum of x and y
+z = x + y
+
+# Print the value of z
+print("Value of z:", z)
+
