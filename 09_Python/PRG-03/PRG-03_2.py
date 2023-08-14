97fd1bf18bc6d1577dac12b5697e20e2a342feaf
techgroundsStudent <135595563+techgroundsStudent@users.noreply.github.com>
2023-08-14 15:33:31 +0200

    PS

--- 09_Python/PRG-03/PRG-03_2.py
+++ 09_Python/PRG-03/PRG-03_2.py
@@ -1,6 +1,7 @@
 #Use the input() function to get input from the user. Store that input in a variable.
 doeEensMee = input("Geef eens wat data in de vorm van een vermakelijke zin met correcte syntax: ")
 print(doeEensMee)
+#Find out what data type the output of input() is. See if it is different for different kinds of input (numbers, words, etc.).
 print(type(doeEensMee))
 
 
