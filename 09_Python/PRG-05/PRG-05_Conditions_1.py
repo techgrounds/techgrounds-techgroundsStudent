9ddaba7cee73006f98da7468dd42e2a9cfdaf5bb
techgroundsStudent <135595563+techgroundsStudent@users.noreply.github.com>
2023-08-15 11:25:31 +0200

    PRG-05

---
+++ 09_Python/PRG-05/PRG-05_Conditions_1.py
@@ -0,0 +1,1 @@
+# Use the input() function to ask the user of your script for their name. If the name they input is your name, print a personalized welcome message. If not, print a different personalized message.
