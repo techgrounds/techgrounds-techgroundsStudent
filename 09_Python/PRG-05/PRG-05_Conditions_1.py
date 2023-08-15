30754d7bfec0f6d10c9ec70c4b48276a7dd1bfde
techgroundsStudent <135595563+techgroundsStudent@users.noreply.github.com>
2023-08-15 12:01:42 +0200

    Conditions

--- 09_Python/PRG-05/PRG-05_Conditions_1.py
+++ 09_Python/PRG-05/PRG-05_Conditions_1.py
@@ -1,1 +1,7 @@
 # Use the input() function to ask the user of your script for their name. If the name they input is your name, print a personalized welcome message. If not, print a different personalized message.
+naampje = input("Vertel eens hoe jij heet:")
+if naampje == "Vincent":
+    print("Welkom thuis, verloren zoon")
+else:
+    print("Good riddance!")
+       
