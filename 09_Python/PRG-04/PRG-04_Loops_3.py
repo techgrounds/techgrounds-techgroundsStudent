3e74f6a55dcbddab5c51abc7162295ab30eeb675
techgroundsStudent <135595563+techgroundsStudent@users.noreply.github.com>
2023-08-15 11:22:58 +0200

    afr.

--- 09_Python/PRG-04/PRG-04_Loops_3.py
+++ 09_Python/PRG-04/PRG-04_Loops_3.py
@@ -1,4 +1,4 @@
-# Use a for loop to loop over the array. Print every name individually.
+# Use a for loop to loop over the array. Print every name individually. https://homepage.tudelft.nl/b3k1c/python/00_00_Intro.html
 arr = ["Coen", "Casper", "Joshua", "Abdessamad", "Saskia"]
 
 for t in arr:
