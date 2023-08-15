22c4b6b96b745642b7a39b77845c6e0fb16af04d
techgroundsStudent <135595563+techgroundsStudent@users.noreply.github.com>
2023-08-15 15:15:12 +0200

    start

---
+++ 09_Python/PRG-06/PRG-06 Functions_3.py
@@ -0,0 +1,9 @@
+#Write the custom function avg() so that it returns the average of the given parameters. You are not allowed to edit any code below the second comment.
+def avg():
+
+
+# you are not allowed to edit any code below here
+x = 128
+y = 255
+z = avg(x,y)
+print("The average of",x,"and",y,"is",z)
