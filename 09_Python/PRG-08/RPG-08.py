a88203ef3fdbfaacc9c48757c75fb872b83cb84b
techgroundsStudent <135595563+techgroundsStudent@users.noreply.github.com>
2023-08-16 13:53:11 +0200

    Add files via upload

---
+++ 09_Python/PRG-08/RPG-08.py
@@ -0,0 +1,10 @@
+    # Create a dictionary
+werknemer = {"voornaam": "Casper", 
+             "achternaam": "Van Velzen",
+             "functie": "LC",
+             "bedrijf": "TechGrounds"
+}
+for x in werknemer:
+    y = werknemer[x]
+    print(x, ":", y)
+
