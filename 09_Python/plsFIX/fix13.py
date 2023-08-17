6c890b26924b0d9b7920a55b6cc0f3e17521a150
techgroundsStudent <135595563+techgroundsStudent@users.noreply.github.com>
2023-08-17 14:39:29 +0200

    fix13isDone

---
+++ 09_Python/plsFIX/fix13.py
@@ -0,0 +1,8 @@
+'''
+The output should be:
+Your random number is: <insert random number here>
+'''
+import random
+
+tLot = random.randint(1,100) #variable ingevoegd
+print(f"Your random number is:{tLot}") #middels een f-string de variable in een printje
