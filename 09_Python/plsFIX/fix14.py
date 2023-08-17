9cdd475a8c31844697e4595aae4eba65d0fdcd91
techgroundsStudent <135595563+techgroundsStudent@users.noreply.github.com>
2023-08-17 14:49:12 +0200

    fix14done

---
+++ 09_Python/plsFIX/fix14.py
@@ -0,0 +1,16 @@
+'''
+The output should be:
+True
+'''
+def rtn(x):
+	return(x)
+
+foo = rtn(3)
+
+if foo < rtn(4):
+	print(True)
+else:
+	print(False)
+'''
+De uitkomst moest TRUE zijn. Dit bereikt door in regel 10 ">" te vervangen door "<". 
+'''
