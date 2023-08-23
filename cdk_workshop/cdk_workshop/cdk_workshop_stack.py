0954f27b9e58198a065ffbb184963b9e7abdb9d4
Techgrounds Student <vdewindt@gmail.com>
2023-08-23 15:30:39 +0200

    cdk_workshop

--- cdk_workshop/cdk_workshop/cdk_workshop_stack.py
+++ cdk_workshop/cdk_workshop/cdk_workshop_stack.py
@@ -1,26 +1,12 @@
 from constructs import Construct
 from aws_cdk import (
-    Duration,
-    Stack,
-    aws_iam as iam,
-    aws_sqs as sqs,
-    aws_sns as sns,
-    aws_sns_subscriptions as subs,
+    Stack
 )
 
 
 class CdkWorkshopStack(Stack):
 
-    def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
-        super().__init__(scope, construct_id, **kwargs)
+    def __init__(self, scope: Construct, id: str, **kwargs) -> None:
+        super().__init__(scope, id, **kwargs)
 
-        queue = sqs.Queue(
-            self, "CdkWorkshopQueue",
-            visibility_timeout=Duration.seconds(300),
-        )
-
-        topic = sns.Topic(
-            self, "CdkWorkshopTopic"
-        )
-
-        topic.add_subscription(subs.SqsSubscription(queue))
+        # Nothing here!
