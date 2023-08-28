96331efc801c21ca9e0f5b5e9e784f9ce4dd9366
techgroundsStudent <135595563+techgroundsStudent@users.noreply.github.com>
2023-08-28 17:02:21 +0200

    privateCloud

--- ProductVersie1/product_versie1/PrivateCloud.py
+++ ProductVersie1/product_versie1/PrivateCloud.py
@@ -1,6 +1,13 @@
-import aws_cdk as cdk 
-from aws_cdk import aws_ec2 as ec2 
+import aws_cdk as cdk
+from aws_cdk import aws_ec2 as ec2
+
+class CloudStack(cdk.Stack):
+    def __init__(self, scope: cdk.Construct, id: str, **kwargs) -> None:
+        super().__init__(scope, id, **kwargs)
+
+        vpc = ec2.Vpc(self, "PrivateWolk", max_azs=2)
 
 
-vpc = ec2.Vpc(self, "PriveWolk", max_azs=3)
+
+
 
