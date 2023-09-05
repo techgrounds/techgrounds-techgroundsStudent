0b0b6b2c19550b64193c3c891d02b27ca4b6a272
techgroundsStudent <135595563+techgroundsStudent@users.noreply.github.com>
2023-09-05 17:09:51 +0200

    Tuesday laatste werkzaamheden

--- ProductVersie1/product_versie1/deCloud_stack.py
+++ ProductVersie1/product_versie1/deCloud_stack.py
@@ -1,6 +1,7 @@
 import aws_cdk as cdk
 from aws_cdk import (
     Stack,
+    CfnOutput,
     aws_ec2 as ec2,
     aws_s3 as s3, 
     aws_kms as kms,
@@ -14,7 +15,7 @@ class deCloud(Stack):
         super().__init__(scope, id, **kwargs)
 
        
-    #   Maak een s3-bucket die encrypted is 
+        #   Maak een s3-bucket die encrypted is 
         deEmmer = s3.Bucket(self, "VersleuteldeEmmer",
         bucket_name = "s3-bucket4scripts",
         access_control = s3.BucketAccessControl.PRIVATE,
@@ -45,12 +46,21 @@ class deCloud(Stack):
         sg_webserver.add_ingress_rule(ec2.Peer.ipv4('10.20.20.0/24'), ec2.Port.tcp(22), "SSH toegang voor de adminServer")
         
      
-        # Creëer een webserver binnen "app-prd-vpc" die draait op Windows         
+        # Creëer een webserver binnen "app-prd-vpc" die draait op linux         
         app_server = ec2.Instance(self, "app_server", 
                                   vpc = vpc_app, 
                                   instance_type = ec2.InstanceType("t3a.micro"),
-                                  machine_image = ec2.MachineImage.latest_windows(ec2.WindowsVersion.WINDOWS_SERVER_2019_ENGLISH_FULL_BASE),
-                                  security_group = sg_webserver
+                                  machine_image = ec2.MachineImage.latest_amazon_linux(
+                                    generation = ec2.AmazonLinuxGeneration.AMAZON_LINUX_2), 
+                                  security_group = sg_webserver,
+                                  user_data = ec2.UserData.for_linux(shebang =  """#!/bin/bash
+                                    yum -y install httpd
+                                    systemctl enable httpd
+                                    systemctl start httpd
+                                    echo '<html><h1>L.S., MOGE UW TOCHT NAAR DEZE PLAATS VOORSPOEDIG ZIJN GEWEEST </h1></html>' >
+                                    /var/www/html/index.html"""
+                                    )
+                                               
         )
         # webserver moet via http en https toegankelijk zijn 
         app_server.connections.allow_from_any_ipv4(ec2.Port.tcp(80), "Wereldwijd toegankelijk")
@@ -63,9 +73,10 @@ class deCloud(Stack):
         vpc_admin_server = ec2.Vpc(self, "management-prd-vpc", 
                             nat_gateways = 0,
                             max_azs = 2,
-                            ip_addresses = ec2.IpAddresses.cidr("10.20.20.0/24")
-                                     
-                                                          )
+                            ip_addresses = ec2.IpAddresses.cidr("10.20.20.0/24"),
+                            subnet_configuration = [ec2.SubnetConfiguration(name="Publiek_Admin",subnet_type = ec2.SubnetType.PUBLIC,)]                            
+                            
+        )
         
         # Creëer een SG voor de admin-server          
         sg_admin_server = ec2.SecurityGroup(self,"sgAdminServer", 
@@ -73,25 +84,40 @@ class deCloud(Stack):
                                          description = "sg_admin_server vanuit CDK",
                                          allow_all_outbound = True,
                                          disable_inline_rules = False,
-                                    )
-        sg_admin_server.add_ingress_rule(ec2.Peer.ipv4('0.0.0.0/0'), ec2.Port.tcp(22), "SSH toegang naar de adminServer van overal") #dit voor nu, aangezien ik niet weer wat de ip-adressen zijn. 
+        )
+        sg_admin_server.add_ingress_rule(ec2.Peer.ipv4('86.87.153.240/32'), ec2.Port.tcp(22), "SSH toegang naar de adminServer van overal") #dit voor nu, aangezien ik niet weer wat de ip-adressen zijn. 
 
         
         # Creëer een vpc-peering connection in je infrastructuur         
         Cloud_Peering = ec2.CfnVPCPeeringConnection(self, "De_Gewenste_Peering_der_Clouds",
          peer_vpc_id = "management-prd-vpc",
         vpc_id = "app-prd-vpc",
-         )   
+        )   
              
         # Creëer een management-server binnen de VPC management-prd-vpc, deze zal werken op Linux     
-        admin_server = ec2.instance(self, "admin_server", 
+        admin_server = ec2.Instance(self, "admin_server", 
                                     instance_type = ec2.InstanceType("t3a.micro"), 
                                     machine_image = ec2.MachineImage.latest_amazon_linux(
                                         generation = ec2.AmazonLinuxGeneration.AMAZON_LINUX_2), 
                                     vpc = vpc_admin_server,
                                     security_group = sg_admin_server                                    
-                                    ),
-        # Creëer een backup systeem in AWS voor het bedrijf         
+        ),
+        # Creëer een backup van de webserver waarbij de backups 7 dagen behouden moeten blijven 
+        
+        plan_BU_app = backup.BackupPlan(self, "plan_der_BU_app"
+                                        )
+        BU_regel_wekelijks = backup.BackupPlanRule.weekly()
+        
+        plan_BU_app.add_rule(BU_regel_wekelijks)
+        plan_BU_app.add_selection(id = "louter_de_app_server", 
+                                  resources = [backup.BackupResource.from_construct(app_server)
+                                               ] 
+                                  )
+        
+        
+        
+        
+                
         # Implementeer KMS in je infrastructuur         
         # IAM gebruiken in je infrastructuur
         
